d118e902af00ee5eb5127b20561092577f1cc5dc
aminacherop <cheropamina26@gmail.com>
2025-04-18 17:06:22 +0300

    router code-Along

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+function About (){
+    return (
+        <>
+        <header>
+            <main>
+                <h1>This is my about component!</h1>
+            </main>
+        </header>
+        </>
+    );
+};
+export default About
